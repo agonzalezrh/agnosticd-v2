6f07796615b887b30b4ba594dcae57afdba4cbcb
Wolfgang Kulhanek <wkulhanek@users.noreply.github.com>
2025-10-13 17:19:30 +0200

    Change check for env_type to config (#58)
    * Change check for env_type to config
    
    * Rename yamllint
    
    * Rename yamllint back

--- tests/static/syntax-check.sh
+++ tests/static/syntax-check.sh
@@ -80,22 +80,22 @@ do_ansible_syntax() {
 
     config=$(basename $(dirname "${i}"))
 
-    if ! egrep --quiet ^env_type: ${i}; then
-        echo "No env_type found in ${i}"
+    if ! egrep --quiet ^config: ${i}; then
+        echo "No config found in ${i}"
     fi
-    env_type=$(egrep ^env_type: ${i}|cut -d' ' -f 2)
+    config=$(egrep ^config: ${i}|cut -d' ' -f 2)
 
     # Ansible Workshops AKA as Linklight needs to be downloaded
-    if [ "${env_type}" = linklight ] || [ "${env_type}" = ansible-workshops ] || [ "${env_type}" = aap2-ansible-workshops ]; then
-        if [ ! -d ${ansible_path}/workdir/${env_type} ]; then
+    if [ "${config}" = linklight ] || [ "${config}" = ansible-workshops ] || [ "${config}" = aap2-ansible-workshops ]; then
+        if [ ! -d ${ansible_path}/workdir/${config} ]; then
             set +e
 
             git clone --branch devel \
                 https://github.com/ansible/workshops.git \
-                ${ansible_path}/workdir/${env_type} &> $output
+                ${ansible_path}/workdir/${config} &> $output
 
             if [ $? = 0 ]; then
-                commit=$(cd ${ansible_path}/workdir/${env_type}; PAGER=cat git show --no-patch --format=oneline --no-color)
+                commit=$(cd ${ansible_path}/workdir/${config}; PAGER=cat git show --no-patch --format=oneline --no-color)
                 echo "OK .......... ${item} / Download ansible-workshop -- $commit"
             else
                 echo "FAIL ........ ${item} / Download ansible-workshop"
@@ -112,8 +112,8 @@ do_ansible_syntax() {
         )
     fi
 
-    if [ -e "${ansible_path}/configs/${env_type}/hosts" ]; then
-        inventory=(-i "${ansible_path}/configs/${env_type}/hosts")
+    if [ -e "${ansible_path}/configs/${config}/hosts" ]; then
+        inventory=(-i "${ansible_path}/configs/${config}/hosts")
     else
         inventory=(-i "${static}/tox-inventory.txt")
     fi
